05e85a9d2de2babf0f5dfe451235277ba88b6109
eltoncox <elton.unix@gmail.com>
2023-04-21 18:36:34 -0300

    gerar token JWT, e classes de suporte.

--- src/main/java/com/elton/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/com/elton/med/voll/api/controller/AutenticacaoController.java
@@ -1,6 +1,9 @@
 package com.elton.med.voll.api.controller;
 
 import com.elton.med.voll.api.domain.usuario.DadosAutenticacao;
+import com.elton.med.voll.api.domain.usuario.Usuario;
+import com.elton.med.voll.api.infra.security.DadosTokenJWT;
+import com.elton.med.voll.api.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +21,16 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha() );
-        var authentication =  manager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha() );
+        var authentication =  manager.authenticate(authenticationToken);
 
-        return  ResponseEntity.ok().build();
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+        return  ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 }
